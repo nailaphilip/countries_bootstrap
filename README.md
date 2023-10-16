ad52a16e0141a10a9e33db31e92d818af90fed2f
nailyaphilippo <nailya@sall.com>
2023-10-16 14:52:01 +0300

    more styling added

--- README.md
+++ README.md
@@ -1,3 +1,110 @@
-# Getting Started with Create React App
+# React Advanced Course Project
 
-This is 
+Welcome to the React Advanced Course project! This application was built using React, React Router Dom, React Redux, and React Redux Toolkit. It also includes authentication functionality and is styled using Bootstrap. In this README, you will find instructions on how to set up and use the application.
+
+## Table of Contents
+
+1. [Getting Started](#getting-started)
+   - [Prerequisites](#prerequisites)
+   - [Installation](#installation)
+2. [Usage](#usage)
+3. [Features](#features)
+4. [Contributing](#contributing)
+5. [License](#license)
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, make sure you have the following software installed on your system:
+
+- [Node.js](https://nodejs.org/) - The runtime environment for running JavaScript applications.
+- [Git](https://git-scm.com/) - Version control system for managing project files.
+
+### Installation
+
+Follow these steps to set up the project:
+
+1. Clone the repository to your local machine using Git:
+
+   ```bash
+   git clone https://github.com/nailaphilip/countries_bootstrap.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd countries_bootstrap
+   ```
+
+3. Install project dependencies using npm or yarn:
+
+   ```bash
+   npm install
+   # or
+   yarn install
+   ```
+
+4. Create a `.env` file in the project's root directory with the following environment variables:
+
+   ```env
+   REACT_APP_OPENWEATHER_KEY=your_openweather_key
+   ```
+
+   Replace `your_openweather_key` with the appropriate values for your API setup.
+
+5. Start the development server:
+
+   ```bash
+   npm start
+   # or
+   yarn start
+   ```
+
+6. The application will be accessible at [http://localhost:3000](http://localhost:3000).
+
+## Usage
+
+Once the application is up and running, you can start using it. Here are some of the main features and how to use them:
+
+### Authentication
+
+- The application includes user authentication. You can sign up or log in with your credentials.
+- Access to certain features or routes may be restricted based on the user's authentication status.
+
+### Navigation
+
+- The React Router Dom is used for navigation. You can explore different routes and pages of the application.
+- Here's an example of navigating to a different route within the application:
+
+  ```javascript
+  import { Link } from "react-router-dom";
+
+  // Inside your component
+  <Link to="/dashboard">Go to Dashboard</Link>;
+  ```
+
+### Redux State Management
+
+- Redux and React Redux Toolkit are used for state management in the application.
+- You can access and manage application state using Redux. Example:
+
+  ```javascript
+  import { useSelector, useDispatch } from "react-redux";
+
+  // Access state
+  const someData = useSelector((state) => state.someSlice.someData);
+
+  // Dispatch actions
+  const dispatch = useDispatch();
+  dispatch(someAction());
+  ```
+
+## Features
+
+- List the key features of your application here.
+- Include any additional information about the functionality of your app.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import { Card, Col, ListGroup } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { LinkContainer } from 'react-router-bootstrap';
-import { addFavourite, removeFavourite } from '../features/countries/favouritesSlice';
+import { Card, Col, ListGroup } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { addFavourite, removeFavourite } from "../features/countries/favouritesSlice";
+import './component.css'
 
 const CountryCard = ({ country }) => {
   const favouritesList = useSelector((state) => state.favourites.favourites);
   const dispatch = useDispatch();
 
-  return (
-    <Col className="mt-5">
-      <LinkContainer
-        to={`/countries/${country.name.common}`}
-        state={{ country: country }}
-      >
-        <Card className="h-100">
-        {favouritesList.includes(country.name.common) ? (
+    return (
+            <Col className="mt-5">
+             
+                <Card className="h-100 component">
+                  {favouritesList?.includes(country.name.common) ? (
                     <i
                     className="bi bi-heart-fill text-danger m-1 p-1"
-                    onClick={() => dispatch(removeFavourite(country.name.common))} />
+                    onClick={() => dispatch(removeFavourite(country.name.common))}></i>
                   ) : (
                     <i
                     className="bi bi-heart text-danger m-1 p-1"
-                    onClick={() => dispatch(addFavourite(country.name.common))} />
+                    onClick={() => dispatch(addFavourite(country.name.common))}></i>
                   )}
-        <Card.Img
+                    <LinkContainer
+                 to={`/countries/${country.name.common}`}
+                 state={{ country: country }}
+               >
+                <div>
+                <Card.Img
                     variant="top"
                     src={country.flags.svg}
                     className="rounded h-50"
@@ -34,44 +36,39 @@ const CountryCard = ({ country }) => {
                       maxHeight: "200px",
                     }}
                   />
-          <Card.Body className="d-flex flex-column">
-            {/* <Card.Img src={country.flags.svg} alt={country.name.common} /> */}
-            <Card.Title>{country.name.common}</Card.Title>
-            <Card.Subtitle className="mb-5 text-muted">
-              {country.name.official}
-            </Card.Subtitle>
-            <ListGroup
-              variant="flush"
-              className="flex-grow-1 justify-content-end"
-            >
-              {country.languages ? (
-                <ListGroup.Item>
-                  <i className="bi bi-translate me-2"></i>
-                  {Object.values(country.languages).join(", ")}
-                </ListGroup.Item>) : (
-                <ListGroup.Item>No language found</ListGroup.Item>
-              )}
-              {country.currencies ? (
-                <ListGroup.Item>
-                  <i className="bi bi-cash-coin me-2"></i>
-                  {Object.values(country.currencies)
-                    .map((currency) => currency.name)
-                    .join(", ")}
-                </ListGroup.Item>
-              ) : (
-                <ListGroup.Item>No currency data</ListGroup.Item>
-              )}
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{country.name.common}</Card.Title>
+                    <Card.Subtitle className="mb-5 text-muted">
+                      {country.capital}
+                    </Card.Subtitle>
+                    <ListGroup
+                      variant="flush"
+                      className="flex-grow-1 justify-content-end"
+                    >
+                      <ListGroup.Item>
+                        <i className="bi bi-translate me-2">
+                          {Object.values(country.languages ?? {}).join(", ")}
+                        </i>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <i className="bi bi-cash-coin me-2">
+                          {Object.values(country.currencies ?? {})
+                            .map((currency) => currency.name)
+                            .join(", ")}
+                        </i>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <i className="bi bi-people me-2">
+                          {country.population.toLocaleString()}
+                        </i>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                  </div>
+              </LinkContainer>
+                </Card>
+            </Col>
+    )
+}
 
-              <ListGroup.Item>
-                <i className="bi bi-people me-2"></i>
-                {country.population}
-              </ListGroup.Item>
-            </ListGroup>
-          </Card.Body>
-        </Card>
-      </LinkContainer>
-    </Col>
-  );
-};
-
-export default CountryCard;
+export default CountryCard
